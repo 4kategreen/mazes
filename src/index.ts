d9f899d1d36aee71f39965d024d73c6b58f400d1
Kate Green <kate@kategreen.us>
2021-02-21 13:58:38 -0500

    creating a function to generate the maze so we can generate more of them

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 const { Grid, recursiveBacktracker } = require('./layout');
 
 let grid = new Grid(20,20);
-recursiveBacktracker(grid);
-let maze = document.getElementById('maze');
-if (maze) {
-  maze.innerText = grid.print()
-} else {
-  document.body.innerText = 'No maze to display';
-}
 
+const generateMaze = (grid: typeof Grid) => {
+  recursiveBacktracker(grid);
+  let maze = document.getElementById('maze');
+  if (maze) {
+    maze.innerText = grid.print()
+  } else {
+    document.body.innerText = 'No maze to display';
+  }
+};
+
+generateMaze(grid);
