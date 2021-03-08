e2ac50e39e8249a615da9626cb115de5222ba6d5
Kate Green <kate@kategreen.us>
2021-03-07 22:18:59 -0500

    generating canvas size at page load for responsiveness moving a whole bunch of stuff to html writing a little documentation inline (should move to readme)

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,29 @@
-const { Walls, createWalls, createMaze } = require('./layout');
-
-const generateMaze = (height: number, width: number): void => {
-  let myMaze = createWalls(height, width);
-  return createMaze(myMaze);
-};
+const { Walls, createMaze } = require('./layout');
 
 const generateNewMaze = (): void => {
   let height = document.getElementsByName("height")[0],
       width = document.getElementsByName("width")[0],
       canvas = document.getElementsByTagName("canvas")[0];
 
-  let myMaze = generateMaze(parseInt(height.value),parseInt(width.value))
+  let myMaze = createMaze(parseInt(height.value),parseInt(width.value))
   printWalls(myMaze, canvas);
 }
-
-const createInput = (elementName: string, userText: string): HTMLLabelElement => {
-    let label = document.createElement("label"),
-        input = document.createElement('input');
-    
-    label.innerText = userText;
-    input.setAttribute("name", elementName);
-    input.setAttribute("type", "number");
-    input.setAttribute("placeholder", "20");
-    input.required = true;
-    input.min="1";
-    label.appendChild(input);
-
-    return label;
-  }
 
 const initialize = (): void => {
   let maze = document.getElementById('maze');
 
-  let canvas = document.createElement("canvas"),
-      button = document.createElement("button"),
-      form = document.createElement("form");
+  let button = document.getElementById("regenerateSubmit"),
+      form = document.getElementById("regenerate"),
+      canvas = document.getElementById("mazeDrawing");
 
-  canvas.height = 500;
-  canvas.width = 500;
+  canvas.height = canvas.parentElement.clientWidth * .9;
+  canvas.width = canvas.parentElement.clientWidth * .9;
 
   form.addEventListener("submit", (e) => e.preventDefault(), true);
-
-  button.innerText = "Generate New Maze!";
   button.addEventListener("click", generateNewMaze);
 
-  let height = createInput("height", "Height"),
-      width = createInput("width", "Width");
-
   if (maze) {
-    form.appendChild(height);
-    form.appendChild(width);
-    form.appendChild(button);
-    maze.appendChild(form);
-    maze.appendChild(canvas);
-
-    let myMaze = generateMaze(10,10)
+    let myMaze = createMaze(50,50)
     printWalls(myMaze, canvas);
   }
 }
--- src/layout.ts
+++ src/layout.ts
@@ -1,33 +1,34 @@
-// given a block, the walls should be 	latitude = [bx,by][bx+1,by]
-// 																		longitude = [bx,by][bx, by+1]
-
-// given a 5x5 grid, lat walls should be 5 r and 6 c
-// 									 long walls should be 6 r and 5 c		
-
-// joins
-// 0,0 -> 1,0 (1,0 wall); 1,6 -> 2,6 (2,6 wall)
-// 0,0 -> 0,1 (0,1 wall)
-// one number is the same. first number==long change, second number==lat change
-// the wall is always the larger of the two coordinates
-
-// find out wall numbers
-// 0,0 -> lat 0,1 (not lat 0,0); long 1,0 (not long 0,0)
-// 1,0 -> lat 1,0 and 2,0; long 1,1 (not 1,0)
-// 2,1 -> lat 2,1 and 3,1; long 2,1 and 2,2
+/**
+ * Walls
+ *  - -
+ * | | |
+ *  - -
+ * | | |
+ *  _ _
+ *
+ * All are based on an (x,y) coordinates pattern and expressed as (row, column) or (r,c)
+ * Walls are expressed as latitude and longitude
+ * Latitude walls have an extra column (3 v. 2 cells)
+ * Longitude walls have an extra row
+ *
+ * Cell 0,0 -> lat 0,1 (lat 0,0 is a wall); long 1,0 (long 0,0, wall)
+ * Cell 1,0 -> lat 1,0 and 2,0; long 1,1 (not 1,0, wall)
+ * Cell 2,1 -> lat 2,1 and 3,1; long 2,1 and 2,2
+ **/
 
 type Walls = {
 	rows: number,
 	columns: number,
-	latitude: boolean[][],
-	longitude: boolean[][]
+	latitude: boolean[][], // rows, columns+1
+	longitude: boolean[][] // rows+1, columns
 }
-type Block = {
+type Cell = {
 	row: number,
 	column: number
 };
 type Wall = [number, number, string];
 
-function createWalls(rows: number, columns: number): Walls {
+function createMaze(rows: number, columns: number): Walls {
 	let walls: Walls = {
 		rows: rows,
 		columns: columns,
@@ -37,7 +38,6 @@ function createWalls(rows: number, columns: number): Walls {
 
 	for (let r=0; r<walls.rows; r++) {
 		walls.latitude[r] = [];
-
 		for (let c=0; c<=walls.columns; c++) {
 			walls.latitude[r][c] = true;
 		}
@@ -45,28 +45,21 @@ function createWalls(rows: number, columns: number): Walls {
 
 	for (let r=0; r<=walls.rows; r++) {
 		walls.longitude[r] = [];
-
 		for (let c=0; c<walls.columns; c++) {
 			walls.longitude[r][c] = true;
 		}
 	}
 
-	makeEntrance(walls);
-
-	return walls;
+	return recursiveBacktracker(walls);
 };
 
-function makeEntrance(w: Walls): void {
-	w.longitude[0][0] = false;
-	// w.longitude[4][5] = false;
-}
-function createMaze(w: Walls): Walls {
-	let stack: Block[] = [];
+function recursiveBacktracker(w: Walls): Walls {
+	let stack: Cell[] = [];
 	stack.push(randomCell(w.rows, w.columns))
 
 	while (stack.length > 0) {
-		let current: Block = stack[stack.length -1];
-		let nextCell: Block|null = getNextCell(current, w);
+		let current: Cell = stack[stack.length -1];
+		let nextCell: Cell|null = moveToNextCell(current, w);
 
 		if (nextCell === null) {
 			stack.pop();
@@ -82,29 +75,33 @@ function createMaze(w: Walls): Walls {
 		}
 	}
 
+	// entrance and exit
+	w.longitude[0][0] = false;
+	w.longitude[w.rows][w.columns-1] = false;
+
 	return w;
 };
 
-function getNextCell(cell: Block, walls: Walls): Block|null {
-	let neighbors: Block[] = [];
+function moveToNextCell(cell: Cell, walls: Walls): Cell|null {
+	let neighbors: Cell[] = [];
 
-	if (cell.row > 0)
+	if (cell.row > 0) // north neighbor
 		neighbors.push({ row: cell.row-1, column: cell.column })
-	if (cell.row+1 < walls.rows)
+	if (cell.row+1 < walls.rows) // south neighbor
 		neighbors.push({ row: cell.row+1, column: cell.column })
-	if (cell.column > 0)
+	if (cell.column > 0) // west neighbor
 		neighbors.push({ row: cell.row, column: cell.column-1 })
-	if (cell.column+1 < walls.columns)
+	if (cell.column+1 < walls.columns) // east neighbor
 		neighbors.push({ row: cell.row, column: cell.column+1 })
 
-	let availableCells = neighbors.filter(n => getLinks(n, walls) === 0);
+	// find cells that are all surrounded by walls, or not linked
+	let availableCells = neighbors.filter(n => getCellLinks(n, walls) === 0);
 
 	if (availableCells.length === 0) return null;
-
 	return availableCells[Math.floor(Math.random()*availableCells.length)]
 }
 
-function getLinks(cell:Block, walls: Walls): number {
+function getCellLinks(cell:Cell, walls: Walls): number {
 	let links = 0;
 
 	if (!walls.latitude[cell.row][cell.column]) links++;
@@ -116,30 +113,35 @@ function getLinks(cell:Block, walls: Walls): number {
 	return links;
 }
 
-function randomCell(rows: number, columns: number): Block {
+function randomCell(rows: number, columns: number): Cell {
 	return {
 		row: Math.floor(Math.random()*rows), 
 		column: Math.floor(Math.random()*columns)
 	}
 };
 
-function findWall(b1: Block, b2: Block): Wall {
-	let removal: Wall = [0, 0, 'unknown'];
+// joins
+// 0,0 -> 1,0 (1,0 wall); 1,6 -> 2,6 (2,6 wall)
+// 0,0 -> 0,1 (0,1 wall)
+// one number is the same. first number==long change, second number==lat change
+// the wall is always the larger of the two coordinates
+function findWall(b1: Cell, b2: Cell): Wall {
+	let singleWall: Wall = [0, 0, 'unknown'];
 
 	if (b1.row === b2.row) {
-		removal[2] = 'latitude';
-		removal[0] = b1.row;
-		removal[1] = b1.column > b2.column ? b1.column : b2.column;
+		singleWall[2] = 'latitude';
+		singleWall[0] = b1.row;
+		singleWall[1] = b1.column > b2.column ? b1.column : b2.column;
 	} else if (b1.column === b2.column) {
-		removal[2] = 'longitude';
-		removal[0] = b1.row > b2.row ? b1.row : b2.row;
-		removal[1] = b1.column;
+		singleWall[2] = 'longitude';
+		singleWall[0] = b1.row > b2.row ? b1.row : b2.row;
+		singleWall[1] = b1.column;
 	} else {
 		throw new Error('unknown wall type');
 	}
 
-	return removal; 
+	return singleWall; 
 };
 
 
-export { Walls, createWalls, createMaze }
+export { Walls, createMaze, recursiveBacktracker }
