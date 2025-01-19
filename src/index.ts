cdc6eef46275a18ed12868836fa7fa859f8dd33f
Kate Green <kate@kategreen.us>
2025-01-19 15:58:48 -0500

    updating the ts declarations, reordering the functions, renaming a few classes

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,51 @@
 import { createMaze } from './layout';
 import { Walls, WallOptions } from './types';
 
-const toggleMazeSize = (clicked): void => {
-  let size = clicked.target;
+const initialize = (): void => {
+  let body = document.getElementById('body') as HTMLDivElement;
 
-  let removeActive = size.parentElement.parentElement.getElementsByClassName('is-active');
-  removeActive[0].removeAttribute('class', 'is-active');
-  size.parentElement.setAttribute('class', 'is-active');
+  let regenerateSubmit = document.getElementById("regenerateSubmit") as HTMLButtonElement,
+      printMazeSubmit = document.getElementById("printMaze") as HTMLButtonElement,
+      regenerate = document.getElementById("regenerate") as HTMLFormElement,
+      canvas = document.getElementById("mazeDrawing") as HTMLCanvasElement,
+      canvasParent = canvas.parentElement as HTMLDivElement,
+      difficulty = document.getElementById("difficultyToggle") as HTMLDivElement;
 
-  switch (size.innerText) {
-    case 'Hard':
-      document.getElementsByName("height")[0].value = 80;
-      document.getElementsByName("width")[0].value = 100;
-      break;
-    case 'Medium':
-      document.getElementsByName("height")[0].value = 40;
-      document.getElementsByName("width")[0].value = 50;
-      break;
-    case 'Easy': // set in instantiation
-    default:
-      document.getElementsByName("height")[0].value = 20;
-      document.getElementsByName("width")[0].value = 25;
-      break;
+  // set event listeners
+  regenerateSubmit.addEventListener("click", generateNewMaze);
+  printMazeSubmit.addEventListener("click", printMaze);
+  // @ts-ignore
+  printMazeSubmit.addEventListener("click", gtag_report_conversion); 
+  difficulty.addEventListener("click", toggleMazeSize)
+  regenerate.addEventListener("submit", (e) => e.preventDefault(), true);
+
+  // set canvas to screen size
+  canvas.height = canvasParent.clientWidth * .95;
+  canvas.width = canvasParent.clientWidth * .95;
+
+  if (body) {
+    let myMaze = createMaze(40, 40);
+    printWalls(myMaze, canvas);
   }
-
-  generateNewMaze()
 }
 
 const generateNewMaze = (): void => {
-  let height = document.getElementsByName("height")[0],
-      width = document.getElementsByName("width")[0],
-      canvas = document.getElementsByTagName("canvas")[0];
+  let height = document.getElementsByName("height")[0] as HTMLInputElement,
+      width = document.getElementsByName("width")[0] as HTMLInputElement,
+      canvas = document.getElementsByTagName("canvas")[0] as HTMLCanvasElement;
 
   let myMaze = createMaze(parseInt(height.value),parseInt(width.value))
+
   printWalls(myMaze, canvas);
 }
 
 const printMaze = (): void => {
-    let win=window.open(),
-        canvas = document.getElementById("mazeDrawing");
+    let win=window.open() as Window,
+        canvas = document.getElementById("mazeDrawing") as HTMLCanvasElement;
 
     win.document.write("<h2>mazemonster.com</h2><p>Free printable mazes. Generate as many mazes as you want.</p>")
     win.document.write("<br><img src='"+canvas.toDataURL()+"'/>");
     win.print();
-}
-
-const initialize = (): void => {
-  let maze = document.getElementById('maze');
-
-  let regenerateSubmit = document.getElementById("regenerateSubmit"),
-      printMazeSubmit = document.getElementById("printMaze"),
-      regenerate = document.getElementById("regenerate"),
-      canvas = document.getElementById("mazeDrawing"),
-      difficulty = document.getElementById("difficultyToggle");
-
-  regenerateSubmit.addEventListener("click", generateNewMaze);
-  printMazeSubmit.addEventListener("click", printMaze);
-  printMazeSubmit.addEventListener("click", gtag_report_conversion);
-  difficulty.addEventListener("click", toggleMazeSize)
-  regenerate.addEventListener("submit", (e) => e.preventDefault(), true);
-  canvas.height = canvas.parentElement.clientWidth * .95;
-  canvas.width = canvas.parentElement.clientWidth * .95;
-
-  if (maze) {
-    let myMaze = createMaze(40, 40);
-    printWalls(myMaze, canvas);
-  }
 }
 
 const printWalls = (w: Walls, canvas: HTMLCanvasElement): void => {
@@ -77,8 +57,8 @@ const printWalls = (w: Walls, canvas: HTMLCanvasElement): void => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = 'black';
 
-    for (let r=0;r<w.numRows;r++) {
-      for (let c=0;c<=w.numColumns;c++) {
+    for (let r=0; r<w.numRows; r++) {
+      for (let c=0; c<=w.numColumns; c++) {
         if (w.longitude[r][c] !== WallOptions.Open) {
           let stationary = c*longUnit,
               start = r*latUnit,
@@ -108,5 +88,35 @@ const printWalls = (w: Walls, canvas: HTMLCanvasElement): void => {
     }
   }
 };
+
+const toggleMazeSize = (clicked: Event): void => {
+  let size = clicked.target as HTMLUListElement,
+      sizeParent = size.parentElement as HTMLUListElement,
+      allSizes = sizeParent.parentElement as HTMLDivElement,
+      height = document.getElementsByName("height")[0] as HTMLInputElement,
+      width = document.getElementsByName("width")[0] as HTMLInputElement,
+      removeActive = allSizes.getElementsByClassName('is-active')[0];
+
+  removeActive.removeAttribute('class');
+  sizeParent.setAttribute('class', 'is-active');
+
+  switch (size.innerText) {
+    case 'Hard':
+      height.value = "80";
+      width.value = "100";
+      break;
+    case 'Medium':
+      height.value = "40";
+      width.value = "50";
+      break;
+    case 'Easy': 
+    default:
+      height.value = "20";
+      width.value = "25";
+      break;
+  }
+
+  generateNewMaze()
+}
 
 initialize()
